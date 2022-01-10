fc5f96688821c55d332f7f5c9882e4a9d35087b8
yoonk1228 <yoonk1228@naver.com>
2022-01-10 17:51:03 +0900

    수정 및 변경

---
+++ class/202201/20220110/practice.js
@@ -0,0 +1,5 @@
+console.log('확인')
+
+const clickBtn = document.querySelector('#btn')
+clickBtn.addEventListener('click', () => console.log('hello world!'))
+clickBtn.addEventListener('dblclick', () => console.log('hello world! *double'))
